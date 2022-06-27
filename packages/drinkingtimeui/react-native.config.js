e52e78b70f61e8a9f8f336550f94661faab26d61
Brian Scaturro <scaturrob@gmail.com>
2022-06-27 16:33:36 -0400

    chore(drinkingtimeui): react-native config changes

--- packages/drinkingtimeui/react-native.config.js
+++ packages/drinkingtimeui/react-native.config.js
@@ -1,10 +1,15 @@
 module.exports = {
-  project: {
+  platform: {
     ios: {},
     android: {},
   },
-  dependency: {
-    assets: ['assets'],
-  },
-  assets: ['./assets/fonts/'],
+  assets: [
+    './assets/fonts/AbrilFatface-Regular.ttf',
+    './assets/fonts/InriaSans-Bold.ttf',
+    './assets/fonts/InriaSans-BoldItalic.ttf',
+    './assets/fonts/InriaSans-Italic.ttf',
+    './assets/fonts/InriaSans-Light.ttf',
+    './assets/fonts/InriaSans-LightItalic.ttf',
+    './assets/fonts/InriaSans-Regular.ttf',
+  ],
 };

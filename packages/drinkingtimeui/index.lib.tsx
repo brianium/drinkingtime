8ffbe5a9e49f49a688648d26432b7307fe820bb4
Brian Scaturro <scaturrob@gmail.com>
2022-07-01 10:40:26 -0400

    chore(drinkingtimeui): add exports for info and text input to main export

--- packages/drinkingtimeui/index.lib.tsx
+++ packages/drinkingtimeui/index.lib.tsx
@@ -1,5 +1,7 @@
 export {default as Button} from './components/Button';
 export {default as Drink} from './components/Drink';
+export {default as Info} from './components/Info';
 export {default as SafeAreaView} from './components/SafeAreaView';
 export {default as Text} from './components/Text';
+export {default as TextInput} from './components/TextInput';
 export {default as HomeScreen} from './screens/Home';
